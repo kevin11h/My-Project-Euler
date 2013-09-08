ce6cb5438ada827bacd8a74973b42eda2a109659
kevin11h <kevhung11@gmail.com>
2013-09-08 04:18:43 -0700

    problem 16 is asking for the sum of digits, not the actual exponentiated value

--- problem-16-power-digit-sum/power_digit_sum.py
+++ problem-16-power-digit-sum/power_digit_sum.py
@@ -1,3 +1,8 @@
 from bignatural import BigNatural
 
-print BigNatural("2") ** BigNatural("1000")
+result_as_string = str(BigNatural("2") ** BigNatural("1000"))
+
+result_as_int_list = map(int, ''.join(result_as_string))
+
+sum_of_digits = sum(result_as_int_list)
+print sum_of_digits
