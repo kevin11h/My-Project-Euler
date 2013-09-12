7ced145a168e68394d4adfb7cb217c7d893a3407
kevin11h <kevhung11@gmail.com>
2013-09-11 18:42:32 -0700

    fixed off-by-one errors in problem 19 script

--- problem-19-counting-sundays/counting_sundays.py
+++ problem-19-counting-sundays/counting_sundays.py
@@ -61,10 +61,10 @@ class Date:
     def to_day_number(self):
         day_count = 0
 
-        for mm in range(1, self.month + 1):
+        for mm in range(1, self.month):
             day_count += days_in_month(mm, self.year)
 
-        day_count += self.day 
+        day_count += self.day
 
         return day_count
 
@@ -84,13 +84,13 @@ class Date:
                 mm = MONTHS.index(month_name) + 1
                 return Date(dd, mm, yyyy)
             else:
-                day_count += end_day_number - start_day_number
+                day_count += days_in_month(MONTHS.index(month_name) + 1, yyyy) 
 
     def days_away_from(self, date2):
         dates = sorted([self, date2], key=lambda d: (d.year, d.month, d.day))
-        end_date, start_date = dates
+        start_date, end_date = dates
 
-        if start_date.year is end_date.year:
+        if start_date.year == end_date.year:
             return end_date.to_day_number() - start_date.to_day_number()
         else:
             day_count = 0
@@ -98,16 +98,19 @@ class Date:
             for year in range(start_date.year + 1, end_date.year):
                 day_count += days_in_year(year)
 
-            day_count += days_in_year(start_date.year) - start_date.to_day_number()
-            day_count += end_date.to_day_number()
+            day_count += days_in_year(start_date.year) - start_date.to_day_number() + 1
+            day_count += end_date.to_day_number() - 1
 
             return day_count
+
+    def __repr__(self):
+        return repr("-".join(map(str,[self.month, self.day, self.year])))
 
 def find_num_sundays_that_fall_on_first_of_each_month_between_years():
     YEAR_START = 1901
     YEAR_END = 2000
-    REFERENCE_DATE = Date(11, 9, 2013)
-    REFERENCE_DAY_OF_WEEK = "WED"
+    REFERENCE_DATE = Date(31, 12, 2013)
+    REFERENCE_DAY_OF_WEEK = "TUE"
     TARGET_DAY = "SUN"
 
     num_matches = 0
@@ -124,6 +127,4 @@ def find_num_sundays_that_fall_on_first_of_each_month_between_years():
     return num_matches
 
 print find_num_sundays_that_fall_on_first_of_each_month_between_years()
-            
-            
-        
+
